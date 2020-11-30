eaa55eb88c697a2d40e2b01426be6432564c9c34
Maciek <appscode4u@gmail.com>
2020-11-30 10:15:39 +0100

    first commit

---
+++ InterfacesExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterfacesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
